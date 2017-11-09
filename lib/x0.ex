815f7b7af24c248d8c7668afc2af3b8e1bbe3948
Arkadiusz Plichta <Arkadiusz.Plichta@powel.com>
2017-11-09 10:41:32 +0100

    init

---
+++ lib/x0.ex
@@ -0,0 +1,18 @@
+defmodule X0 do
+  @moduledoc """
+  Documentation for X0.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> X0.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/x0_test.exs
@@ -0,0 +1,8 @@
+defmodule X0Test do
+  use ExUnit.Case
+  doctest X0
+
+  test "greets the world" do
+    assert X0.hello() == :world
+  end
+end
